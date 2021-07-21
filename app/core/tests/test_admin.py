c856683133b51f8f0c2fdd7ecd8b9b1317b3adc5
Yanti Suryati <yantisc@gmail.com>
2021-07-21 22:38:27 +0100

    setup django admin

---
+++ app/core/tests/test_admin.py
@@ -0,0 +1,28 @@
+from django.test import TestCase, Client
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+class AdminSiteTests(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email = 'admin@myemail.com',
+            password = 'password123'
+
+        )
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            email = 'test@myemail.com',
+            password = 'password123',
+            name = 'Test user full name'
+
+        )
+
+    def test_user_listed(self):
+        """test that users are listed on user page"""
+        url = reverse('admin:core_user_changelist')
+        res = self.client.get(url)
+
+        self.assertContains(res, self.user.name)
+        self.assertContains(res, self.user.email)
